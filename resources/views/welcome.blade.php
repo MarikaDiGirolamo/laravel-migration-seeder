72e1a43ca407e0fcf25c9e2ec538f6249589c0f0
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-06-29 19:35:30 +0200

    foreach

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container my-3">
-    <h1>Welcome Page</h1>
-    <div class="row g-4">
-        <div class="col">
-            <div>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, delectus ad esse illum omnis earum eligendi sint a minus quasi, inventore nulla autem. Maxime voluptatem eligendi veniam voluptates. Soluta, sunt!</p>
+    <div class="container my-3">
+        <h1>Welcome Page</h1>
+        <div class="row g-4">
+            <div class="col">
+                <div>
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col">#id</th>
+                                <th scope="col">Company</th>
+                                <th scope="col">Departure</th>
+                                <th scope="col">Arrival</th>
+                                <th scope="col">Departure Time</th>
+                                <th scope="col">Arrival Time</th>
+                                <th scope="col">Train Code</th>
+                                <th scope="col">Carriage</th>
+                                <th scope="col">On Time</th>
+                                <th scope="col">Deleted</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($trains as $train)
+                                <tr>
+                                    <th scope="row">{{ $train->id }}</th>
+                                    <td>{{ $train->company }}</td>
+                                    <td>{{ $train->departure_station }}</td>
+                                    <td>{{ $train->arrival_station }}</td>
+                                    <td>{{ $train->departure_time }}</td>
+                                    <td>{{ $train->arrival_time }}</td>
+                                    <td>{{ $train->train_code }}</td>
+                                    <td>{{ $train->train_carriages }}</td>
+                                    <td>{{ $train->on_time }}</td>
+                                    <td>{{ $train->deleted }}</td>
+                                </tr>
+                            @endforeach
+
+                        </tbody>
+                    </table>
+
+                </div>
             </div>
         </div>
-    </div>
 
-</div>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\PageController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,12 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $links = config('store.someLinks');
-    return view('welcome', compact('links'));
-});
+Route::get('/', [PageController::class, 'index']);
 
-Route::get('/otherpage', function () {
-    $links = config('store.someLinks');
-    return view('other', compact('links'));
-});
+// Route::get('/otherpage', function () {
+//     $links = config('store.someLinks');
+//     return view('other', compact('links'));
+// });
